b830bc2afcf3d7356a75a4faa1c3d69ea859b3d5
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2024-03-18 11:48:58 +0530

    first commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var image = document.querySelector(".navbar-brand img");
 window.addEventListener("scroll", () => {
     nav.classList.toggle("active", window.scrollY > 0)
     if (window.scrollY > 0) {
-        image.src = "img/alogo.png";
+        image.src = "img/alogo.svg";
     }
     else {
         image.src = "img/alogo2.png";
@@ -38,3 +38,30 @@ $(document).ready(function () {
 var swiper = new Swiper(".swiper-wrapper", {
     slidesPerView: 3,
 })
+// Get the slideshow container element
+const slideshowContainer = document.getElementById('slide-slow-box');
+
+// Array of image URLs
+const imageUrls = [
+  'img/imagenani.png',
+  'img/Frame 3.png',
+  'img/Frame 4.png',
+  'img/Frame 5.png',
+];
+
+let currentIndex = 0;
+
+// Function to change the image
+function changeImage() {
+  // Set the background image of the slideshow container
+  slideshowContainer.src = imageUrls[currentIndex];
+
+  // Increment the current index
+  currentIndex = (currentIndex + 1) % imageUrls.length;
+}
+
+// Initial call to change the image
+changeImage();
+
+// Set interval to change the image every 3 seconds
+setInterval(changeImage, 3000);
