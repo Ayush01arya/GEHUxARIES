3898930c4000ee66735a27f505adb47426e2d105
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2024-04-14 10:53:55 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -59,7 +59,7 @@ class HEADER extends HTMLElement {
               <li><a class="dropdown-item" href="logistic_venue.html">Venue</a></li>
               <li><a class="dropdown-item" href="logistic_accommodations.html">Accommodation</a></li>
               <li><a class="dropdown-item" href="#">Travel</a></li>
-              <li><a class="dropdown-item" href="logistic_tour.html">Tour</a></li>
+              <li><a class="dropdown-item" href="logistics_tour.html">Tour</a></li>
             </ul>
           </li>
           <li class="nav-item"><a class="nav-link active" aria-current="page" href="sponsorship.html">Sponsorship</a></li>
